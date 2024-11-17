f5fbaa5a1c45645c6c853e30237666b9c3ccb374
MichaelSRD <michaelsrd02@gmail.com>
2024-11-17 14:32:18 -0500

    cambio de fondo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import BussinesCard from "./components/bussinesCard";
 
 export default function Home() {
   return (
-    <div className=" flex justify-center items-center min-h-screen bg-gray-300 ">
+    <div className=" flex justify-center items-center min-h-screen bg-[#F2F5F9] ">
      <BussinesCard/>
     </div>
   );
